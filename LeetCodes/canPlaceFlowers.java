d7205e498cb8b16b492f2aa4f438e7249dd90679
AJ Williams <alexanderwilliams29@gmail.com>
2026-02-25 11:25:33 -0600

    Create canPlaceFlowers.java

---
+++ LeetCodes/canPlaceFlowers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int planted = 0;
+
+        for(int i = 0; i < flowerbed.length; i++){
+            //Trading memory for readability
+            boolean nextCondition = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+            boolean currentCondition = flowerbed[i] == 0;
+            boolean lastCondition = (i == 0) || (flowerbed[i - 1] == 0);
+
+            //If all conditions are met, plant the flower and set signals.
+            if(nextCondition && currentCondition && lastCondition){
+                flowerbed[i] = 1; 
+                planted++;
+
+                //If we planted a flower, we can't put one in the next slot.
+                // This line's impact : Beats 5% --> Beats 98%
+                i++; 
+            }
+        }
+        
+        return planted >= n;
+    }
+}
